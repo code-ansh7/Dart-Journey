e1c4e7f7902c9a10aca0baae375161960be3cfb7
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-07-07 17:58:31 +0545

    Setup DART compiler on VS Code.

---
+++ CompilerSetupTest.dart
@@ -0,0 +1,10 @@
+void main() {
+  String developerName = "Ansh";
+  String goal = "Professional Flutter App Developer";
+  int codingHours = 4;
+
+  print("👋 Welcome, $developerName!");
+  print("🚀 Goal : $goal");
+  print("💻 Daily Coding Target : $codingHours Hours");
+  print("🔥 Dart Journey Started Successfully!");
+}
