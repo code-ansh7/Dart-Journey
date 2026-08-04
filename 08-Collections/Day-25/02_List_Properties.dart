e5aa85796bd61454fffac646bc5d7ae2bcb38596
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-08-04 21:48:06 +0545

    📊 Day 25: Explored essential list properties like length, first, last, isEmpty, and isNotEmpty

---
+++ 08-Collections/Day-25/02_List_Properties.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  List<String> students = [
+    "Ansh",
+    "Vansh",
+    "Nitin",
+    "Sagar",
+  ];
+
+  print("Students : $students");
+
+  print("");
+
+  print("Length     : ${students.length}");
+  print("First      : ${students.first}");
+  print("Last       : ${students.last}");
+  print("Is Empty   : ${students.isEmpty}");
+  print("Not Empty  : ${students.isNotEmpty}");
+
+}
