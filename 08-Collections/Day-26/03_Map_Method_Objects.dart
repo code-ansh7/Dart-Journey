5053c8c55acb2fe34917e108630551f14dd7fcd5
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-08-07 21:48:04 +0545

    🛍️ Day 26: Applied map() on custom objects to transform product data into a new collection

---
+++ 08-Collections/Day-26/03_Map_Method_Objects.dart
@@ -0,0 +1,34 @@
+class Product {
+  String name;
+  int price;
+
+  Product({
+    required this.name,
+    required this.price,
+  });
+}
+
+void main() {
+
+  List<Product> products = [
+    Product(name: "Laptop", price: 60000),
+    Product(name: "Mouse", price: 500),
+    Product(name: "Keyboard", price: 1500),
+    Product(name: "Monitor", price: 12000),
+  ];
+
+  List<String> productNames = products.map((product) {
+    return product.name;
+  }).toList();
+
+  print("Products List");
+
+  for (Product product in products) {
+    print("${product.name} - ₹${product.price}");
+  }
+
+  print("");
+
+  print("Product Names : $productNames");
+
+}
