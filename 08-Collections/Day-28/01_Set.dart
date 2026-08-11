5041cd6d455118b8c8463fd0c64eadbc9ab163c6
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-08-11 21:58:31 +0545

    📦 Day 28: Introduced Dart Set collection with unique student registration data

---
+++ 08-Collections/Day-28/01_Set.dart
@@ -0,0 +1,8 @@
+void main(){
+  
+  Set<String> registeredStudent = {"Ansh", "Rahul", "Ansh", "Vansh", "Rahul", "Sagar"};
+
+ print("Registered Student : ${registeredStudent}");
+ print("Number of Students : ${registeredStudent.length}");
+
+}
